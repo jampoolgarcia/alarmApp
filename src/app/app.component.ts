efaf7842ea6432de094ec0f73a6b551a14600d98
jampool <jampoolgarcia@gmail.com>
2020-11-15 15:46:03 -0400

    create component alarm clock and add config component main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'alarmApp';
+  seconds = 0;
+  /*alarmMessage = "even sleeping the alarm does not sound..."*/
+  alarmMessage = "get up it's time to work...."
+
+  getRandomSeconds(){
+    this.seconds = Math.trunc(Math.random()*60);
+  }
+
 }
